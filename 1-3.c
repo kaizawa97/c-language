d563d8dc56455fe42d89d357b5deb589c8b7cd2b
kaizawawa <kai109870@gmail.com>
2019-09-24 11:44:15 +0900

    "1-3 9/24"

--- 1-3.c
+++ 1-3.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 
-int main(void) {
-	float fahr, celsius;
-	int lower,upper, step;
+int main()
+{
+    float fahr, celsius;
+    int lower, upper, step;
 
-	lower = 0;
-	upper = 300;
-	step = 20;
-	fahr = lower;
+    lower = 0;
+    upper = 300;
+    step = 20;
+    char c = 'C';
+    char f = 'F';
 
-	while (fahr <= upper) {
-		celsius = (5.0/9.0) * (fahr - 32.0);
-		printf("%3.0f  ％6.1\n",fahr , celsius);
-		fahr = fahr +step;
-	}
+    printf("%3c %6c \n", c, f);
+    fahr = lower;
+    while (fahr <= upper)
+    {
+        celsius = (5.0 / 9.0) * (fahr - 32.0);
+        printf("%3.0f %6.1f\n", fahr, celsius);
+        fahr = fahr + step;
+    }
 }
-
-
