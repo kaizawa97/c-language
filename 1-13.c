c6b0f7e7a7d8d852ef9f6afa9d0d96e559ef8af6
kaizawawa <41991049+zawakun@users.noreply.github.com>
2019-10-01 18:57:31 +0900

    Update 1-13.c

--- 1-13.c
+++ 1-13.c
@@ -1,46 +1,43 @@
-#include <stdio.h>
-
-#define MAX 100
-
-int main()
-{
-
-	int c, i, len, list_count, max, n, view;
-	int length = 0;
-	int count[MAX];
-
-	len = 0;
-	list_count = 0;
-	view = 0;
-
-	while (((c = getchar())) != EOF)
-	{
-		if (c >= '0' && c <= '9')
-		{
-			count[list_count] = len;
-			++list_count;
-			len = 0;
-		}
-		if (c == ' ' || c == '\n' || c == '\t')
-		{
-			count[list_count] = len;
-			++list_count;
-			len = 0;
-		}
-	}
-	printf("%d\n", list_count);
-
-	for (i = 0; i < list_count; ++i)
-	{
-		c = count[i];
-		++view;
-	}
-	// for (i = 0; i < MAX; ++i)
-	// {
-	// 	printf("%d\n", i);
-	// 	for (n = 0; n < view; ++n)
-	// 	{
-	// 		putchar('*');
-	// 	}
-	// }
-}
+#include <stdio.h>                                                                                                                
+ #define MAX 100
+ 
+ int main()
+   {
+     int max[MAX];
+     int listview[MAX];
+     int c,i,n,list_count,len; 
+    len = 0;
+    list_count = 0;
+  
+  
+    while (((c = getchar())) != EOF)
+  {
+     if (c == ' ' || c == '\n' || c == '\t')
+     {
+      max[list_count] = len;
+      ++list_count;
+      len = 0;
+     }
+     else {
+      ++len;
+     }
+     
+    }
+    printf("%d\n", list_count);
+ 
+//    for (i = 0; i < list_count; ++i)
+//    {
+//     c = max[i];
+//     ++listview;
+//    }
+    for (i = 0; i < MAX; ++i)
+    {
+     printf("%d\n", i);
+    for (n = 0; n < max[i]; ++n)
+    {
+     putchar('*');
+     printf("\n");
+    }
+    }
+   }
+                                
